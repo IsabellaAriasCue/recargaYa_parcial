6c072e0015480ba2e776f204f866e6c85b43d7a9
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 17:17:06 -0500

    Se realizo el refactor que fue posible

--- src/recargaya/recargas.py
+++ src/recargaya/recargas.py
@@ -2,11 +2,11 @@ def calcular_recarga(monto, premium=False):
     if not 1000 <= monto <= 50000:
         return "rechazado"
 
-    bono = 0
-
     if monto >= 30000:
         bono = 0.25
     elif monto >= 10000:
         bono = 0.10
+    else:
+        bono = 0
 
     return monto * (1 + bono)
