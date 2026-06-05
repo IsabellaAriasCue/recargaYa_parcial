f606088ae01896d06010307989bf42ddff91905b
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 17:20:54 -0500

    test: premium agrega 5% adicional

--- tests/test_recargas.py
+++ tests/test_recargas.py
@@ -7,3 +7,5 @@ def test_recarga_invalida_menor_1000():
 def test_bono_10_por_ciento():
     assert calcular_recarga(10000) == 11000
 
+def test_premium():
+    assert calcular_recarga(10000, True) == 11550
