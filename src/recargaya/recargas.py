2e6a7e65f5e9b6a5094382c09ad45b1a890c9246
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 17:14:37 -0500

    feat: implementación bono 10% y 25%

--- src/recargaya/recargas.py
+++ src/recargaya/recargas.py
@@ -2,4 +2,11 @@ def calcular_recarga(monto, premium=False):
     if not 1000 <= monto <= 50000:
         return "rechazado"
 
-    return monto
+    bono = 0
+
+    if monto >= 30000:
+        bono = 0.25
+    elif monto >= 10000:
+        bono = 0.10
+
+    return monto * (1 + bono)
